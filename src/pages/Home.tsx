d124011609e9b5381d154928ede8bddcdcc3b8cd
m4rcin7 <mrku1337@gmail.com>
2025-03-15 17:04:56 +0100

    :movie-app

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -26,17 +26,24 @@ export function Home() {
   };
 
   return (
-    <main className="max-[1400px]">
-      <div className="mt-10 flex flex-wrap gap-5 justify-center items-center">
+    <main className="container mx-auto py-10 px-4">
+      <h1 className="text-4xl font-bold text-center mb-12">Popularne Filmy</h1>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
         {movieList.map((film) => (
-          <div key={film.id}>
+          <div
+            key={film.id}
+            className="group relative overflow-hidden rounded-lg shadow-lg transform transition-transform duration-300 hover:scale-105"
+          >
             {film.poster_path && (
               <img
-                className="hover:scale-[1.1] transition ease-in-out cursor-pointer"
-                src={`https://image.tmdb.org/t/p/w200${film.backdrop_path}`}
-                alt={`${film.title}`}
+                className="w-full h-auto object-cover"
+                src={`https://image.tmdb.org/t/p/w500${film.poster_path}`} // Zwiększona rozdzielczość
+                alt={film.title}
               />
             )}
+            <div className="absolute inset-0 bg-black bg-opacity-40 opacity-0 group-hover:opacity-100 transition-opacity duration-300 flex items-center justify-center">
+              <p className="text-white text-lg font-semibold">{film.title}</p>
+            </div>
           </div>
         ))}
       </div>
